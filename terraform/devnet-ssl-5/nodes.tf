589b45b7cefe31e45812a27c16d9b627fa28d964
SunnysidedJ <j@testinprod.io>
2025-07-06 06:32:34 +0000

    Mixed devnet for Fusaka-devnet-2

---
+++ terraform/devnet-ssl-5/nodes.tf
@@ -0,0 +1,340 @@
+# Bootnode
+variable "bootnode" {
+  default = {
+    name            = "bootnode"
+    count           = 1
+    validator_start = 0
+    validator_end   = 0
+    region          = "ams3"
+  }
+}
+
+# Lighthouse
+variable "lighthouse_geth" {
+  default = {
+    name            = "lighthouse-geth"
+    count           = 8
+    validator_start = 0
+    validator_end   = 64
+  }
+}
+
+variable "lighthouse_besu" {
+  default = {
+    name            = "lighthouse-besu"
+    count           = 8
+    validator_start = 64
+    validator_end   = 128
+  }
+}
+
+variable "lighthouse_nethermind" {
+  default = {
+    name            = "lighthouse-nethermind"
+    count           = 8
+    validator_start = 128
+    validator_end   = 192
+  }
+}
+
+variable "lighthouse_nimbusel" {
+  default = {
+    name            = "lighthouse-nimbusel"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lighthouse_erigon" {
+  default = {
+    name            = "lighthouse-erigon"
+    count           = 8
+    validator_start = 192
+    validator_end   = 256
+  }
+}
+
+variable "lighthouse_reth" {
+  default = {
+    name            = "lighthouse-reth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Prysm
+variable "prysm_geth" {
+  default = {
+    name            = "prysm-geth"
+    count           = 8
+    validator_start = 256
+    validator_end   = 320
+  }
+}
+
+variable "prysm_besu" {
+  default = {
+    name            = "prysm-besu"
+    count           = 8
+    validator_start = 320
+    validator_end   = 384
+  }
+}
+
+variable "prysm_nethermind" {
+  default = {
+    name            = "prysm-nethermind"
+    count           = 8
+    validator_start = 384
+    validator_end   = 448
+  }
+}
+
+variable "prysm_nimbusel" {
+  default = {
+    name            = "prysm-nimbusel"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "prysm_erigon" {
+  default = {
+    name            = "prysm-erigon"
+    count           = 8
+    validator_start = 448
+    validator_end   = 512
+  }
+}
+
+variable "prysm_reth" {
+  default = {
+    name            = "prysm-reth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Lodestar
+variable "lodestar_geth" {
+  default = {
+    name            = "lodestar-geth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lodestar_nethermind" {
+  default = {
+    name            = "lodestar-nethermind"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lodestar_besu" {
+  default = {
+    name            = "lodestar-besu"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lodestar_nimbusel" {
+  default = {
+    name            = "lodestar-nimbusel"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lodestar_erigon" {
+  default = {
+    name            = "lodestar-erigon"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lodestar_reth" {
+  default = {
+    name            = "lodestar-reth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Nimbus
+variable "nimbus_geth" {
+  default = {
+    name            = "nimbus-geth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_besu" {
+  default = {
+    name            = "nimbus-besu"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_nethermind" {
+  default = {
+    name            = "nimbus-nethermind"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_nimbusel" {
+  default = {
+    name            = "nimbus-nimbusel"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_erigon" {
+  default = {
+    name            = "nimbus-erigon"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_reth" {
+  default = {
+    name            = "nimbus-reth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Teku
+variable "teku_geth" {
+  default = {
+    name            = "teku-geth"
+    count           = 8
+    validator_start = 512
+    validator_end   = 576
+  }
+}
+
+variable "teku_besu" {
+  default = {
+    name            = "teku-besu"
+    count           = 8
+    validator_start = 576
+    validator_end   = 640
+  }
+}
+
+variable "teku_nethermind" {
+  default = {
+    name            = "teku-nethermind"
+    count           = 8
+    validator_start = 640
+    validator_end   = 704
+  }
+}
+
+variable "teku_nimbusel" {
+  default = {
+    name            = "teku-nimbusel"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "teku_erigon" {
+  default = {
+    name            = "teku-erigon"
+    count           = 8
+    validator_start = 704
+    validator_end   = 768
+  }
+}
+
+variable "teku_reth" {
+  default = {
+    name            = "teku-reth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "grandine_geth" {
+  default = {
+    name            = "grandine-geth"
+    count           = 8
+    validator_start = 768
+    validator_end   = 832
+  }
+}
+
+variable "grandine_besu" {
+  default = {
+    name            = "grandine-besu"
+    count           = 8
+    validator_start = 832
+    validator_end   = 896
+  }
+}
+
+variable "grandine_nethermind" {
+  default = {
+    name            = "grandine-nethermind"
+    count           = 8
+    validator_start = 896
+    validator_end   = 960
+  }
+}
+
+variable "grandine_nimbusel" {
+  default = {
+    name            = "grandine-nimbusel"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "grandine_erigon" {
+  default = {
+    name            = "grandine-erigon"
+    count           = 8
+    validator_start = 960
+    validator_end   = 1024
+  }
+}
+
+variable "grandine_reth" {
+  default = {
+    name            = "grandine-reth"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
